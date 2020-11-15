891c5c35cf9ad616d2f51742210371e46739997f
simon brahan <tntpiranha@gmail.com>
2020-11-15 14:14:07 +0000

    render placed counters

--- src/index.js
+++ src/index.js
@@ -2,63 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+    render() {
+        console.log(this.props.counter);
+        if (this.props.counter) {
+            return (
+                <div className="square">
+                    <div className={`counter counter-${this.props.counter}`} />
+                </div>
+            );
+        }
+
+        return (<div className="square" />);
+    }
+}
+
 class Board extends React.Component {
     render() {
         return (
             <div className="board">
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square counter="yellow" />
+                    <Square />
+                    <Square counter="red" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
                 <div className="board-row">
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
-                    <div className="square" />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
             </div>
         );
