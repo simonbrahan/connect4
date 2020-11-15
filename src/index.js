0b5241161b899c1031768e78d130f359965a6100
simon brahan <tntpiranha@gmail.com>
2020-11-15 13:47:01 +0000

    render game board

--- src/index.js
+++ src/index.js
@@ -1,2 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="board">
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+                <div className="board-row">
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                    <div className="square" />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Board />, document.getElementById('root'));
